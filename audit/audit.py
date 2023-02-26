031446923272414169e95392faff8c6553409bb6
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-02-26 21:59:08 +0000

    Update audit.py

--- audit/audit.py
+++ audit/audit.py
@@ -97,8 +97,8 @@ class Audit(commands.Cog):
         self.invite_regex = re.compile(
             r"(?:https?://)?(?:www\.)?(?:discord\.(?:gg|io|me|li)|(?:discordapp|discord)\.com/invite)/[\w]+"
         )
-        self.whname = "Modmail Audit Logger"
-        self.acname = "modmail-audit"
+        self.whname = "Quality Line Support Audit Logger"
+        self.acname = "📜server-logs"
         self._webhooks = {}
         self._webhook_locks = {}
 
