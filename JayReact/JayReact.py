419d7473c6d47656c3940a33da71f8f3f40ef39f
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-02-28 22:59:07 +0000

    Update JayReact.py

--- JayReact/JayReact.py
+++ JayReact/JayReact.py
@@ -9,7 +9,7 @@ class Jay(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if "JAY" in message.content.upper():
-            await message.add_reaction("OLDER_ADULT")
+            await message.add_reaction("{OLDER_ADULT}")
 
 async def setup(bot):
     await bot.add_cog(Jay(bot))
