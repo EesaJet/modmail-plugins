6c31f82d19a29ae213d37e3ea052a1be144ac85b
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-02-28 22:02:56 +0000

    Update Sun4Kay.py

--- Sun4Kay/Sun4Kay.py
+++ Sun4Kay/Sun4Kay.py
@@ -10,17 +10,6 @@ class Kay(commands.Cog):
     async def on_message(self, message):
         if "KAY" in message.content.upper():
             await message.add_reaction("KAYA:813843744385269762")
-            
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if "APPLE" in message.content.upper():
-            await message.add_reaction("APPLE")
-            
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if "IPHONE" in message.content.upper():
-            await message.add_reaction("APPLE")
-
 
 async def setup(bot):
     await bot.add_cog(Kay(bot))
