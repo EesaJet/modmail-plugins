3d0c3b75c4989faf69a56ae7640f181c9754e41d
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-02-28 22:55:30 +0000

    Add files via upload

---
+++ JayReact/JayReact.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+class Jay(commands.Cog):
+    """Reacts with a banana emoji if someone says banana."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "JAY" in message.content.upper():
+            await message.add_reaction("\N{OLDER_ADULT}")
+
+async def setup(bot):
+    await bot.add_cog(Jay(bot))
