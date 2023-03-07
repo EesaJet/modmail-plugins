6b59b7cfd6afe82b1dfcf367dbcc748759820b26
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-07 22:58:48 +0000

    Update Sun4Kay.py

--- Sun4Kay/Sun4Kay.py
+++ Sun4Kay/Sun4Kay.py
@@ -10,6 +10,12 @@ class Kay(commands.Cog):
     async def on_message(self, message):
         if "KAY" in message.content.upper():
             await message.add_reaction("KAYA:813843744385269762")
+        if "SUN" in message.content.upper() and not message.author.bot:
+            await message.channel.send("Here comes the sun!")
+            await message.channel.send("<a:KayA:813843744385269762>")
+        if "MELLOW" in message.content.upper() and not message.author.bot:
+            await message.channel.send("STOP IT MELLOW!")
+            await message.channel.send(":black_cat:")
 
 async def setup(bot):
     await bot.add_cog(Kay(bot))
