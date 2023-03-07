a4486cd734efda49c87a1944f42bab7ac42e8a73
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-07 22:59:08 +0000

    Update EesaReact.py

--- EesaReact/EesaReact.py
+++ EesaReact/EesaReact.py
@@ -9,7 +9,24 @@ class Eesa(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if "EESA" in message.content.upper():
-            await message.add_reaction("AIRPLANE")
+            await message.add_reaction("\N{AIRPLANE}")
+        elif "SHIT" in message.content.upper():
+            await message.add_reaction("SHIT:1080259762266050670")
+        elif "THAMES" in message.content.upper():
+            await message.add_reaction("\N{SAILBOAT}")
+        elif "PHOTO" in message.content.upper():
+            await message.add_reaction("\N{CAMERA}")
+        elif "JAY" in message.content.upper():
+            await message.add_reaction("OLDMAN:1080268375911059517")
+        elif "ETHAN" in message.content.upper():
+            await message.add_reaction("KNIGHT:1080268333976391780")
+        elif "DAFFY" in message.content.upper():
+            await message.add_reaction("\N{CROWN}")
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
+        await ctx.message.delete()
 
 
 async def setup(bot):
