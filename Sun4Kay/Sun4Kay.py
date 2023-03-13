077133a37d734595797a015ee94727e3279549e6
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-13 12:08:57 +0000

    Update Sun4Kay.py

--- Sun4Kay/Sun4Kay.py
+++ Sun4Kay/Sun4Kay.py
@@ -8,9 +8,12 @@ class Kay(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if "KAY" in message.content.upper():
+      
+        exempt_words = ["SUNDAY", "SUNBURY"]
+      
+        if "KAY" in message.content.upper and not message.author.bot():
             await message.add_reaction("KAYA:813843744385269762")
-        if "SUN" in message.content.upper() and not message.author.bot and "SUNDAY" not in message.content.upper():
+        if "SUN" in message.content.upper() and not message.author.bot and all(exemption not in message.content.upper() for exemption in exempt_words):
             await message.channel.send("Here comes the sun!")
             await message.channel.send("<a:KayA:813843744385269762>")
         if "MELLOW" in message.content.upper() and not message.author.bot:
