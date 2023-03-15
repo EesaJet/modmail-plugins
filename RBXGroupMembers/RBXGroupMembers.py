cbab786cc6b5b3c7ef66cb56cdbe7513ed19394a
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-15 20:15:03 +0000

    Update RBXGroupMembers.py

--- RBXGroupMembers/RBXGroupMembers.py
+++ RBXGroupMembers/RBXGroupMembers.py
@@ -1,63 +1,60 @@
-import datetime
-import pytz
-import discord
 from discord.ext import commands, tasks
+import requests
 
-class RobloxGroupCount(commands.Cog):
-    """A Discord bot cog to post the member count of a Roblox group."""
+class RobloxGroup(commands.Cog):
+    """Sends a message to a specified channel with the total number of members in a Roblox group every day at a specified time."""
 
     def __init__(self, bot, group_id, channel_id, check_time):
         self.bot = bot
         self.group_id = 4418793
         self.channel_id = 937837334534172732
-        self.check_time = check_time
-        self.check_group_count.start()
+        self.check_time = datetime.time(hour=20, minute=18)
+        self.member_count = None
 
-    def cog_unload(self):
-        self.check_group_count.cancel()
+    async def send_group_count(self):
+        url = f'https://groups.roblox.com/v1/groups/{self.group_id}'
+        response = requests.get(url)
+        data = response.json()
+        member_count = data['memberCount']
+
+        if member_count != self.member_count:
+            self.member_count = member_count
+            channel = self.bot.get_channel(self.channel_id)
+            await channel.send(f'Total members: {member_count}')
 
     @tasks.loop(hours=24)
     async def check_group_count(self):
-        # Get the member count of the Roblox group
-        url = f'https://groups.roblox.com/v1/groups/{self.group_id}'
-        response = await self.bot.session.get(url)
-        data = await response.json()
-        member_count = data.get('memberCount', 0)
-
-        # Check if the member count has increased since the last check
-        last_member_count = await self.bot.get_cog_data(self, 'last_member_count')
-        if member_count > last_member_count:
-            # Update the last member count
-            await self.bot.set_cog_data(self, last_member_count=member_count)
-
-            # Post the member count in the specified channel
-            channel = self.bot.get_channel(self.channel_id)
-            message = f'The {data["name"]} group now has {member_count:,} members!'
-            await channel.send(message)
+        await self.send_group_count()
 
     @check_group_count.before_loop
     async def before_check_group_count(self):
         await self.bot.wait_until_ready()
-
-        # Convert check_time to the London, UK timezone
+    
+        # Convert check_time to the US/Eastern timezone
         london_tz = pytz.timezone('Europe/London')
         now = datetime.datetime.now(tz=london_tz)
-        next_check = datetime.datetime.combine(now.date(), self.check_time, london_tz)
-
+        check_time = datetime.time(hour=20, minute=18)
+        next_check = datetime.datetime.combine(now, check_time, london_tz)
+    
         # If the next check time has already passed today, add one day to the next check time
         if next_check <= now:
             next_check += datetime.timedelta(days=1)
-
+    
         # Sleep until the next check time
-        await discord.utils.sleep_until(next_check)
-
+        await asyncio.sleep((next_check - now).total_seconds())
+    
         # Send the group count before starting the loop
-        await self.check_group_count()
+        await self.send_group_count()
+    
+        def cog_unload(self):
+            self.check_group_count.cancel()
 
-await def setup(bot):
-    # Replace these with your own values
+async def setup(bot):
+    # Set your Roblox group ID, channel ID, and check time here
     group_id = 4418793
     channel_id = 937837334534172732
-    check_time = datetime.time(hour=20, minute=10)
+    check_time = datetime.time(hour=20, minute=18)
 
-    bot.add_cog(RobloxGroupCount(bot, group_id, channel_id, check_time))
+    cog = RobloxGroup(bot, group_id, channel_id, check_time)
+    bot.add_cog(cog)
+    cog.check_group_count.start()
