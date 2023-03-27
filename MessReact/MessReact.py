c9cdcec22ccc1d80fc6eb25280da229dbb2f90c1
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-27 22:55:49 +0100

    Update MessReact.py

--- MessReact/MessReact.py
+++ MessReact/MessReact.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-class Eesa(commands.Cog):
+class MessReact(commands.Cog):
     """Reacts with a banana emoji if someone says banana."""
 
     def __init__(self, bot):
@@ -12,4 +12,4 @@ class Eesa(commands.Cog):
             await message.add_reaction(r"\N{MEDAL}")
       
 async def setup(bot):
-    await bot.add_cog(Eesa(bot))
+    await bot.add_cog(MessReact(bot))
