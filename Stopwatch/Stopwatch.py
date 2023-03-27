8768114807cd77ad2fe48360cfb45c5cff60869d
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-27 14:59:33 +0100

    Update Stopwatch.py

--- Stopwatch/Stopwatch.py
+++ Stopwatch/Stopwatch.py
@@ -1,5 +1,4 @@
 import datetime
-
 from discord.ext import commands
 
 class Stopwatch(commands.Cog):
@@ -7,7 +6,7 @@ class Stopwatch(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def start(self, ctx):
+    async def say(self, ctx, *, message):
 
         started = "Stopwatch started."
         await ctx.send(started)
