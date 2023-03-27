36287d2f2afd282349563d87bad9af88a5d5d916
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-27 13:03:12 +0100

    Update Stopwatch.py

--- Stopwatch/Stopwatch.py
+++ Stopwatch/Stopwatch.py
@@ -11,34 +11,13 @@ class Stopwatch(commands.Cog):
     async def start(self, ctx):
         """Starts a personal stopwatch for the user."""
         if ctx.author.id in self.timers:
-            message = await ctx.send("You already have a stopwatch running.")
+            arm = "You already have a stopwatch running."
+            message = await ctx.send(arm)
             return message
 
         self.timers[ctx.author.id] = datetime.datetime.now()
-        message = await ctx.send("Stopwatch started.")
-        return message
-
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops the user's personal stopwatch."""
-        if ctx.author.id not in self.timers:
-            message = await ctx.send("You don't have a stopwatch running.")
-            return message
-
-        elapsed_time = datetime.datetime.now() - self.timers[ctx.author.id]
-        del self.timers[ctx.author.id]
-        message = await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time}")
-        return message
-
-    @commands.command()
-    async def time(self, ctx):
-        """Displays the user's elapsed stopwatch time."""
-        if ctx.author.id not in self.timers:
-            message = await ctx.send("You don't have a stopwatch running.")
-            return message
-
-        elapsed_time = datetime.datetime.now() - self.timers[ctx.author.id]
-        message = await ctx.send(f"Elapsed time: {elapsed_time}")
+        started = "Stopwatch started."
+        message = await ctx.send(started)
         return message
 
 def setup(bot):
