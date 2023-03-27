9c1e90c6d0aa8193018dea15f21e517a7580c8f9
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-27 16:29:21 +0100

    Update EesaReact.py

--- EesaReact/EesaReact.py
+++ EesaReact/EesaReact.py
@@ -1,10 +1,13 @@
 from discord.ext import commands
+from datetime import datetime, timedelta
+import math
 
 class Eesa(commands.Cog):
     """Reacts with a banana emoji if someone says banana."""
 
     def __init__(self, bot):
         self.bot = bot
+        self.timers = {}
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -28,6 +31,45 @@ class Eesa(commands.Cog):
         await ctx.send(message)
         await ctx.message.delete()
 
+    @commands.command()
+    async def start(self, ctx):
+        """Starts a personal stopwatch for the user."""
+        if ctx.author.id in self.timers:
+            await ctx.send("You already have a stopwatch running.")
+            return
 
+        self.timers[ctx.author.id] = datetime.now()
+        await ctx.send("Stopwatch started.")
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Stops the user's personal stopwatch."""
+        if ctx.author.id not in self.timers:
+            await ctx.send("You don't have a stopwatch running.")
+            return
+
+        elapsed_time = datetime.now() - self.timers[ctx.author.id]
+        elapsed_seconds = math.ceil(elapsed_time.total_seconds())
+        hours, remainder = divmod(elapsed_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        del self.timers[ctx.author.id]
+        elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
+        await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time_str}")
+
+    @commands.command()
+    async def time(self, ctx):
+        """Displays the user's elapsed stopwatch time."""
+        if ctx.author.id not in self.timers:
+            await ctx.send("You don't have a stopwatch running.")
+            return
+
+        elapsed_time = datetime.now() - self.timers[ctx.author.id]
+        elapsed_seconds = math.ceil(elapsed_time.total_seconds())
+        hours, remainder = divmod(elapsed_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
+        await ctx.send(f"Elapsed time: {elapsed_time_str}")
+
+      
 async def setup(bot):
     await bot.add_cog(Eesa(bot))
