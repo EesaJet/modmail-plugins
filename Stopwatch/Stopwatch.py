817104cb85ca220289d25bb2eca0ea90987d6e47
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-27 12:26:45 +0100

    Update Stopwatch.py

--- Stopwatch/Stopwatch.py
+++ Stopwatch/Stopwatch.py
@@ -1,39 +1,43 @@
-import discord
+import datetime
+
 from discord.ext import commands
-import time
+
 
 class Stopwatch(commands.Cog):
-    """Starts a stopwatch on a command and counts upwards until the same user stops it."""
-
     def __init__(self, bot):
         self.bot = bot
         self.timers = {}
 
     @commands.command()
     async def start(self, ctx):
-        """Starts a personal stopwatch for the user who calls the command."""
+        """Starts a personal stopwatch for the user."""
         if ctx.author.id in self.timers:
-            await ctx.send("You already have a stopwatch running!")
+            await ctx.send("You already have a stopwatch running.")
             return
 
-        self.timers[ctx.author.id] = time.time()
+        self.timers[ctx.author.id] = datetime.datetime.now()
         await ctx.send("Stopwatch started.")
 
     @commands.command()
     async def stop(self, ctx):
-        """Stops the personal stopwatch for the user who calls the command and displays the final time in hours and minutes."""
+        """Stops the user's personal stopwatch."""
         if ctx.author.id not in self.timers:
-            await ctx.send("You don't have a stopwatch running!")
+            await ctx.send("You don't have a stopwatch running.")
             return
 
-        elapsed_time = time.time() - self.timers[ctx.author.id]
+        elapsed_time = datetime.datetime.now() - self.timers[ctx.author.id]
         del self.timers[ctx.author.id]
+        await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time}")
 
-        # Convert elapsed time from seconds to hours and minutes
-        hours, rem = divmod(elapsed_time, 3600)
-        minutes, seconds = divmod(rem, 60)
+    @commands.command()
+    async def time(self, ctx):
+        """Displays the user's elapsed stopwatch time."""
+        if ctx.author.id not in self.timers:
+            await ctx.send("You don't have a stopwatch running.")
+            return
 
-        await ctx.send(f"Stopwatch stopped. Time elapsed: {int(hours):02d}:{int(minutes):02d}:{seconds:.2f}")
+        elapsed_time = datetime.datetime.now() - self.timers[ctx.author.id]
+        await ctx.send(f"Elapsed time: {elapsed_time}")
 
-async def setup(bot):
+def setup(bot):
     bot.add_cog(Stopwatch(bot))
