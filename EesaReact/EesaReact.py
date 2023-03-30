1c6eb7a59f95a5904cd5399858ede51637224ec1
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-03-30 22:22:25 +0100

    Update EesaReact.py

--- EesaReact/EesaReact.py
+++ EesaReact/EesaReact.py
@@ -9,9 +9,10 @@ class Eesa(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.client = AsyncIOMotorClient("mongodb://localhost:27017")
+        self.mongo_uri = mongo_uri
+        self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client["stopwatch_db"]
-        self.collection = self.db["stopwatch_collection"]
+        self.col = self.db["stopwatch_col"]
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -42,47 +43,54 @@ class Eesa(commands.Cog):
     @commands.command()
     async def start(self, ctx):
         """Starts a personal stopwatch for the user."""
-        result = await self.collection.find_one({"user_id": ctx.author.id})
-        if result:
+        user_id = ctx.author.id
+        stopwatch_data = self.col.find_one({"user_id": user_id})
+        if stopwatch_data:
             await ctx.send("You already have a stopwatch running.")
             return
 
-        start_time = datetime.now()
-        await self.collection.insert_one({"user_id": ctx.author.id, "start_time": start_time})
+        start_time = datetime.now(timezone.utc)
+        stopwatch_data = {"user_id": user_id, "start_time": start_time}
+        self.col.insert_one(stopwatch_data)
         await ctx.send("Stopwatch started.")
 
     @commands.command()
     async def stop(self, ctx):
         """Stops the user's personal stopwatch."""
-        result = await self.collection.find_one({"user_id": ctx.author.id})
-        if not result:
+        user_id = ctx.author.id
+        stopwatch_data = self.col.find_one({"user_id": user_id})
+        if not stopwatch_data:
             await ctx.send("You don't have a stopwatch running.")
             return
 
-        start_time = result["start_time"]
-        elapsed_time = datetime.now() - start_time
+        start_time = stopwatch_data["start_time"]
+        end_time = datetime.now(timezone.utc)
+        elapsed_time = end_time - start_time
         elapsed_seconds = math.ceil(elapsed_time.total_seconds())
         hours, remainder = divmod(elapsed_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
-        await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time_str}")
 
-        await self.collection.delete_one({"user_id": ctx.author.id})
+        self.col.delete_one({"user_id": user_id})
+        await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time_str}")
 
     @commands.command()
     async def time(self, ctx):
         """Displays the user's elapsed stopwatch time."""
-        result = await self.collection.find_one({"user_id": ctx.author.id})
-        if not result:
+        user_id = ctx.author.id
+        stopwatch_data = self.col.find_one({"user_id": user_id})
+        if not stopwatch_data:
             await ctx.send("You don't have a stopwatch running.")
             return
 
-        start_time = result["start_time"]
-        elapsed_time = datetime.now() - start_time
+        start_time = stopwatch_data["start_time"]
+        end_time = datetime.now(timezone.utc)
+        elapsed_time = end_time - start_time
         elapsed_seconds = math.ceil(elapsed_time.total_seconds())
         hours, remainder = divmod(elapsed_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
+
         await ctx.send(f"Elapsed time: {elapsed_time_str}")
 
     @commands.command()
