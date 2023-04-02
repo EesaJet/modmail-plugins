a29f650f70035fecb291a7d36cdeec3c73253bee
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-04-02 13:34:24 +0100

    Update EesaReact.py

--- EesaReact/EesaReact.py
+++ EesaReact/EesaReact.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from datetime import datetime, timedelta
 import math
+import json
 import discord
 
 class Eesa(commands.Cog):
@@ -8,7 +9,8 @@ class Eesa(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.timers = {}
+        self.timer_file = "timers.json"
+        self.timers = self.load_timers()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -36,55 +38,64 @@ class Eesa(commands.Cog):
         await ctx.send(message)
         await ctx.message.delete()
 
+    def load_timers(self):
+        try:
+            with open(self.timer_file, "r") as f:
+                timers = json.load(f)
+        except FileNotFoundError:
+            return {}
+        else:
+            # Convert stored timestamps to datetime objects
+            for user_id, timestamp in timers.items():
+                timers[user_id] = datetime.fromisoformat(timestamp)
+            return timers
+
+    def save_timers(self):
+        # Convert datetime objects to ISO formatted strings for JSON serialization
+        timers = {str(user_id): timestamp.isoformat() for user_id, timestamp in self.timers.items()}
+        with open(self.timer_file, "w") as f:
+            json.dump(timers, f)
+
     @commands.command()
     async def start(self, ctx):
         """Starts a personal stopwatch for the user."""
-        if ctx.author.id in self.timers:
+        if str(ctx.author.id) in self.timers:
             await ctx.send("You already have a stopwatch running.")
             return
 
-        self.timers[ctx.author.id] = datetime.now()
+        self.timers[str(ctx.author.id)] = datetime.now()
+        self.save_timers()
         await ctx.send("Stopwatch started.")
 
     @commands.command()
     async def stop(self, ctx):
         """Stops the user's personal stopwatch."""
-        if ctx.author.id not in self.timers:
+        if str(ctx.author.id) not in self.timers:
             await ctx.send("You don't have a stopwatch running.")
             return
 
-        elapsed_time = datetime.now() - self.timers[ctx.author.id]
+        elapsed_time = datetime.now() - self.timers[str(ctx.author.id)]
         elapsed_seconds = math.ceil(elapsed_time.total_seconds())
         hours, remainder = divmod(elapsed_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
-        del self.timers[ctx.author.id]
+        del self.timers[str(ctx.author.id)]
+        self.save_timers()
         elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
         await ctx.send(f"Stopwatch stopped. Elapsed time: {elapsed_time_str}")
 
     @commands.command()
     async def time(self, ctx):
         """Displays the user's elapsed stopwatch time."""
-        if ctx.author.id not in self.timers:
+        if str(ctx.author.id) not in self.timers:
             await ctx.send("You don't have a stopwatch running.")
             return
 
-        elapsed_time = datetime.now() - self.timers[ctx.author.id]
+        elapsed_time = datetime.now() - self.timers[str(ctx.author.id)]
         elapsed_seconds = math.ceil(elapsed_time.total_seconds())
         hours, remainder = divmod(elapsed_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         elapsed_time_str = f"{hours} Hours {minutes} Minutes {seconds} Seconds"
         await ctx.send(f"Elapsed time: {elapsed_time_str}")
-
-    @commands.command()
-    async def giverole(self, ctx, role_name: str):
-        """Gives everyone a specified role."""
-        role = discord.utils.get(ctx.guild.roles, name=role_name)
-        if role is None:
-            await ctx.send(f"Role {role_name} does not exist.")
-        else:
-            for member in ctx.guild.members:
-                await member.add_roles(role)
-            await ctx.send(f"Role {role_name} has been given to everyone.")
       
 async def setup(bot):
     await bot.add_cog(Eesa(bot))
