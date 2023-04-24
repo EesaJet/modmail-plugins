b5de937517114b034eefe8cd4cc7ebeab54bc212
EesaJet <89559265+EesaJet@users.noreply.github.com>
2023-04-24 13:15:48 +0100

    Update Sun4Kay.py

--- Sun4Kay/Sun4Kay.py
+++ Sun4Kay/Sun4Kay.py
@@ -9,6 +9,7 @@ class Kay(commands.Cog):
         self.bot = bot
         self.log_channel_id = 1050914082083053650  # Replace with the ID of the log channel
         self.monitored_channel_ids = [466682606373830657, 455404878202798100, 773002648743706634]  # Replace with the IDs of the channels to monitor
+        self.role_ids = [1002600411099828326, 455190182623313940] # Replace with your desired role IDs
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -56,6 +57,14 @@ class Kay(commands.Cog):
             # Send the embed to the log channel
             log_channel = await self.bot.fetch_channel(self.log_channel_id)
             await log_channel.send(embed=embed)
+            
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        guild = member.guild
+        for role_id in self.role_ids:
+            role = guild.get_role(role_id)
+            if role is not None:
+                await member.add_roles(role)
 
 async def setup(bot):
     await bot.add_cog(Kay(bot))
