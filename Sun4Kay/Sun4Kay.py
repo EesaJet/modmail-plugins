d89f73abc433becda382deabd3a254e71fc3b64b
EesaJet <89559265+EesaJet@users.noreply.github.com>
2024-06-02 20:04:21 +0100

    Update Sun4Kay.py

--- Sun4Kay/Sun4Kay.py
+++ Sun4Kay/Sun4Kay.py
@@ -30,17 +30,13 @@ class Kay(commands.Cog):
         if "STUDIO" in message.content.upper() and not message.author.bot:
             await message.channel.send("\"Fucking Studio 😡\" ~ Kay 2024")
 
-    @commands.Cog.listener()
-    async def on_webhooks_update(self, channel):
-        if channel.id in self.monitored_channel_ids:
-            # Fetch the latest message in the channel
-            async for message in channel.history(limit=1):
-                if message.webhook_id:
-                    # Check if the message is in the shift notifications channel
-                    if message.channel.id == self.shift_notifications_channel_id and not message.author.bot:
-                        role = message.guild.get_role(self.shift_notifications_role_id)
-                        if role:
-                            await message.channel.send(f"{role.mention}")
+        # Check if the message is sent via webhook
+        if message.webhook_id:
+            # Check if the message is in the shift notifications channel
+            if message.channel.id == self.shift_notifications_channel_id and not message.author.bot:
+                role = message.guild.get_role(self.shift_notifications_role_id)
+                if role:
+                    await message.channel.send(f"{role.mention}")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
