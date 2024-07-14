e483cc92de8eebb2ad29800698b81cd150c85d3b
EesaJet <89559265+EesaJet@users.noreply.github.com>
2024-07-14 22:12:24 +0100

    Update EesasEssentials.py

--- EesasEssentials/EesasEssentials.py
+++ EesasEssentials/EesasEssentials.py
@@ -52,9 +52,10 @@ class Essentials(commands.Cog):
     async def post_deadline_message(self, channel):
         self.deadline = self.get_next_monday_midnight()
         unix_timestamp = int(self.deadline.timestamp())
-        message_content = f"The deadline to submit any activity this week is <t:{unix_timestamp}:R>."
+        message_content = f"## The deadline to submit any activity this week is approximately <t:{unix_timestamp}:R>, on (<t:{unix_timestamp}:F>)."
         message = await channel.send(message_content)
         self.deadline_message_id = message.id
+
 
     @tasks.loop(minutes=1)
     async def check_deadline(self):
